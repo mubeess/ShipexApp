0065de52a868589f08c2f36e24952894575b1759
mubeess <mubarakibrahim2015@gmail.com>
2025-03-24 10:07:51 +0100

    test: added a unit test for login screen

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -3,11 +3,38 @@
  */
 
 import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
+import {render, fireEvent} from '@testing-library/react-native';
 import App from '../App';
 
-test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+jest.mock('@shipex/navigation/AppNavigation', () => 'AppNavigation');
+jest.mock(
+  '@shipex/screens/SplashScreen/AnimatedSplashScreen',
+  () => 'AnimatedSplashScreen',
+);
+jest.mock('react-native-toast-message', () => ({
+  show: jest.fn(),
+  hide: jest.fn(),
+  __esModule: true,
+  default: jest.fn().mockImplementation(() => null),
+}));
+
+describe('App', () => {
+  it('should show splash screen initially and then transition to AppNavigation', async () => {
+    const {getByTestId, queryByTestId} = render(<App />);
+
+    // Initially, the splash screen should be visible
+    setTimeout(() => {
+      expect(getByTestId('splash-screen')).toBeTruthy();
+
+      // Simulate the end of the splash screen animation
+      fireEvent(getByTestId('splash-screen'), 'animationEnd');
+
+      // Wait for the splash screen to disappear
+
+      expect(queryByTestId('splash-screen')).toBeNull();
+
+      // Check if AppNavigation is visible
+      expect(getByTestId('app-navigation')).toBeTruthy();
+    }, 6000);
   });
 });
